7637d914939c79a99907d7a6af4298eccded386d
iCoach <dilait01@hs-esslingen.de>
2018-12-17 10:50:05 +0100

    added script to manage net archiving

---
+++ train.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+now=$(date +"%d-%m-%Y-%H-%M")
+
+
+if [ "$1" = "train" ];
+then
+    python main.py
+elif [ "$1" = "train+" ];
+then
+    if [ -d "~/old" ];
+    then
+    echo existing
+    cd ~/old
+    mkdir $now
+    else
+    mkdir ~/old
+    cd ~/old
+    mkdir $now
+    fi
+    cd ~/festium
+    if [ -d "log" ]; then
+    mv log ~/old/$now/log/
+    fi
+    if [ -d "net" ]; then
+    mv net ~/old/$now/net/
+    fi
+    if [ -d "train_logs" ]; then
+    mv train_logs ~/old/$now/train_logs
+    fi
+    python main.py
+elif [ "$1" = "store" ];
+then
+    if [ -d "~/old" ];
+    then
+    echo existing
+    cd ~/old
+    mkdir $now
+    else
+    mkdir ~/old
+    cd ~/old
+    mkdir $now
+    fi
+    cd ~/festium
+    if [ -d "log" ]; then
+    mv log ~/old/$now/log/
+    fi
+    if [ -d "net" ]; then
+    mv net ~/old/$now/net/
+    fi
+    if [ -d "train_logs" ]; then
+    mv train_logs ~/old/$now/train_logs
+    fi   
+elif [ "$1" = "play" ];
+then
+    python3 replay.py
+elif [ "$1" = "del" ];
+then
+    rm -r log
+    rm -r net
+    rm -r train_logs      
+fi  
+
